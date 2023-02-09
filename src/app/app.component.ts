189e497e90f4230dde6c96d67b03e1530c234b9a
Alejandro Valencia <68349791+KOR3A-dev@users.noreply.github.com>
2023-02-09 16:33:55 -0500

    add template styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FlightsService } from './services/flights.service';
+
+/*Interface*/
+export interface Flight {
+  departureStation: string;
+  arrivalStation: string;
+  flightCarrier: string;
+  flightNumber: string;
+  price:number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +21,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NewShore';
+
+  available_flights = new FormGroup({
+    origin: new FormControl(''),
+    destination: new FormControl(''),
+  });
+
+  dataSource!: MatTableDataSource<Flight>;
+  posts: any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private flightsService: FlightsService) {
+
+    this.flightsService.getFlights().subscribe((data:any) => {
+      console.log("flights : " , data);
+      this.posts = data;
+      // Assign the data to the data source for the table to render
+      this.dataSource = new MatTableDataSource(this.posts);
+
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    MatGridListModule,
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
